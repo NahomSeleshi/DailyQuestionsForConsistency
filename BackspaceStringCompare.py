1609fcdf3e3fc2491b32ad917c0204e3bb0000b5
NahomSeleshi <81203642+NahomSeleshi@users.noreply.github.com>
2022-05-01 11:41:42 +0300

    Create BackspaceStringCompare.py

---
+++ BackspaceStringCompare.py
@@ -0,0 +1,19 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s1 = ""
+        t1 = ""
+        for i in range(len(s)):
+            if s[i] == '#':
+                s1 = s1[0:len(s1)-1]
+            else:
+                s1 += s[i]
+        for j in range(len(t)):
+            if t[j] == '#':
+                t1 = t1[0: len(t1)-1]
+            else:
+                t1 += t[j]
+        if s1 == t1:
+            return True
+        return False
+                
+        
