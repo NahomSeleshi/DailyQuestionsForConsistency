ad9b493b2c5e8ab0f6061181bc3b85b331dfa5e6
NahomSeleshi <81203642+NahomSeleshi@users.noreply.github.com>
2022-05-22 07:43:36 +0300

    Update MinimumLinesToRepresentALineChart.py

--- MinimumLinesToRepresentALineChart.py
+++ MinimumLinesToRepresentALineChart.py
@@ -1,3 +1,24 @@
+#This is the code that I edited my first one with some new implementations
+# like using pairwise in the for loop
+
+from fractions import Fraction
+class Solution:
+    def minimumLines(self, stockPrices: List[List[int]]) -> int:
+        slopes = []
+        stockPrices.sort()
+        if len(stockPrices) == 1: return 0
+        
+        lines = 1
+        prevSlope = Fraction(stockPrices[1][1]-stockPrices[0][1], stockPrices[1][0]-stockPrices[0][0])
+        for first, second in pairwise(stockPrices):
+            newGradient = Fraction(second[1]-first[1], second[0]-first[0])
+            if newGradient != prevSlope:
+                prevSlope = newGradient
+                lines +=1
+           
+        return lines
+
+#This is my first code
 from fractions import Fraction
 class Solution:
     def minimumLines(self, stockPrices: List[List[int]]) -> int:
