0a228c5b24774371aa9671a73c40488c023551d0
NahomSeleshi <81203642+NahomSeleshi@users.noreply.github.com>
2022-07-28 22:10:56 +0300

    Update ValidAnagram.py

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -23,3 +23,14 @@ class Solution:
             if not freqOfS[each] == freqOfT[each]:
                 return False
         return True
+#Another different implementation
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = list(s)
+        t = list(t)
+        s.sort()
+        t.sort()
+        if len(s) != len(t): return False
+        for i in range(len(s)):
+            if s[i] != t[i]: return False
+        return True
