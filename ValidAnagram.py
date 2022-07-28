7d4c5874a2b873593211c24ba9ae013a34be58f6
NahomSeleshi <81203642+NahomSeleshi@users.noreply.github.com>
2022-07-28 22:01:09 +0300

    Update ValidAnagram.py

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -11,3 +11,15 @@ class Solution:
                 return False
         return True
         
+#Another implementaion somehow similar to the one above
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freqOfS = Counter(s)
+        freqOfT = Counter(t)
+        for each in freqOfS:
+            if not freqOfS[each] == freqOfT[each]:
+                return False
+        for each in freqOfT:
+            if not freqOfS[each] == freqOfT[each]:
+                return False
+        return True
