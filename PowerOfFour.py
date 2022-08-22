29302b970bfdcd045b85919a0406c05efbc8c855
NahomSeleshi <nahomseleshi43@gmail.com>
2022-08-22 10:23:02 -0400

    PowerOfFour is modified and added to the repo.

--- PowerOfFour.py
+++ PowerOfFour.py
@@ -3,3 +3,13 @@ class Solution:
         if n <= 0: return False
         x = (math.log(n))/(math.log(4))
         return int(x) == x
+# Below is another implementation using DFS
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        def helper(number):
+            if number < 1:
+                return False
+            if number == 1:
+                return True
+            return helper(number/4)
+        return helper(float(n))
