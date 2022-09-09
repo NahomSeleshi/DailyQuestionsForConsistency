1299ca647dfdb04ca4c0a676831111458da81c48
NahomSeleshi <81203642+NahomSeleshi@users.noreply.github.com>
2022-09-09 19:31:54 -0400

    Update TheNumberOfWeakCharactersInTheGame.py

--- TheNumberOfWeakCharactersInTheGame.py
+++ TheNumberOfWeakCharactersInTheGame.py
@@ -1,14 +1,11 @@
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        properties.sort(key = lambda x: (-x[0], x[1]))
         
-        properties.sort(key=lambda x: (-x[0],x[1]))
-        
-        ans = 0
-        curr_max = 0
-        
-        for _, d in properties:
-            if d < curr_max:
-                ans += 1
+        answer, curMaxDef = 0,0
+        for a,d in properties:
+            if d < curMaxDef:
+                answer += 1
             else:
-                curr_max = d
-        return ans
+                curMaxDef = d
+        return answer
