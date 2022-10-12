787edc5d43ad54277ac7abcdf042cc565655a86a
NahomSeleshi <nahomseleshi43@gmail.com>
2022-10-12 17:03:29 -0400

    LowestCommonAncestorOfaBinaryTree.py was updated

--- LowestCommonAncestorOfaBinaryTree.py
+++ LowestCommonAncestorOfaBinaryTree.py
@@ -36,5 +36,28 @@ class Solution:
                     pPointer += 1
                     qPointer += 1
         return TreeNode(pAncestors[pPointer][1])
+
+# This is a very simple solution that I found on discuss
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == p or root == q:
+            return root
+        
+        left = right = None
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        else:
+            return left or right
                  
                  
